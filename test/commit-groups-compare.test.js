63436ab1605fab9e43560ab6cf9140b6ae66c173
Pierre-Denis Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-08-27 03:13:18 -0400

    fix(preset): Fix changelog group title order with emoji

--- test/commit-groups-compare.test.js
+++ test/commit-groups-compare.test.js
@@ -3,38 +3,34 @@ import commitGroupsCompare from './helpers/commit-groups-compare';
 
 test('Return ordered commit groups', t => {
   const commitGroups = [
-    {title: 'Metadata'},
-    {title: 'Documentation'},
-    {title: 'Bug Fixes'},
-    {title: 'Initial'},
-    {title: 'Features'},
+    {title: '📘 Documentation', commits: [{type: 'docs'}]},
+    {title: '🐞 Bug Fixes', commits: [{type: 'fix'}]},
+    {title: 'Features', commits: [{type: 'feat'}]},
   ];
   const compare = commitGroupsCompare({
     typesOrder: ['feat', 'fix', 'docs', 'initial', 'metadata'],
     types: {
-      feat: {title: 'Features', aliases: {initial: {title: 'Initial'}}},
-      fix: {title: 'Bug Fixes', aliases: {metadata: {title: 'Metadata'}}},
+      feat: {title: 'Features'},
+      fix: {title: 'Bug Fixes'},
       docs: {title: 'Documentation'},
     },
   });
 
   t.deepEqual(commitGroups.sort(compare), [
-    {title: 'Features'},
-    {title: 'Bug Fixes'},
-    {title: 'Documentation'},
-    {title: 'Initial'},
-    {title: 'Metadata'},
+    {title: 'Features', commits: [{type: 'feat'}]},
+    {title: '🐞 Bug Fixes', commits: [{type: 'fix'}]},
+    {title: '📘 Documentation', commits: [{type: 'docs'}]},
   ]);
 });
 
 test('Return alphabeticaly ordered commit groups not in "typesOrder" at the end of the list', t => {
   const commitGroups = [
-    {title: 'b-Test'},
-    {title: 'z-Test'},
-    {title: 'Bug Fixes'},
-    {title: 'z-Test'},
-    {title: 'a-Test'},
-    {title: 'Features'},
+    {title: 'b-Test', commits: [{type: 'btest'}]},
+    {title: 'z-Test', commits: [{type: 'ztest'}]},
+    {title: 'Bug Fixes', commits: [{type: 'fix'}]},
+    {title: 'z-Test', commits: [{type: 'ztest'}]},
+    {title: 'a-Test', commits: [{type: 'atest'}]},
+    {title: 'Features', commits: [{type: 'feat'}]},
   ];
   const compare = commitGroupsCompare({
     typesOrder: ['feat', 'fix'],
@@ -48,11 +44,11 @@ test('Return alphabeticaly ordered commit groups not in "typesOrder" at the end 
   });
 
   t.deepEqual(commitGroups.sort(compare), [
-    {title: 'Features'},
-    {title: 'Bug Fixes'},
-    {title: 'a-Test'},
-    {title: 'b-Test'},
-    {title: 'z-Test'},
-    {title: 'z-Test'},
+    {title: 'Features', commits: [{type: 'feat'}]},
+    {title: 'Bug Fixes', commits: [{type: 'fix'}]},
+    {title: 'a-Test', commits: [{type: 'atest'}]},
+    {title: 'b-Test', commits: [{type: 'btest'}]},
+    {title: 'z-Test', commits: [{type: 'ztest'}]},
+    {title: 'z-Test', commits: [{type: 'ztest'}]},
   ]);
 });
